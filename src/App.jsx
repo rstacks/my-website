c86f16b6c7bda75418212c807a0183049bf0111f
rstacks <stacksrob910@gmail.com>
2023-12-30 17:31:28 -0600

    Initial React commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// Should include import for stylesheet
+// import "./styles.css" or something
+//import { useState } from 'react'
+
+function App() {
+  return "Hello world";
+}
+
+export default App
