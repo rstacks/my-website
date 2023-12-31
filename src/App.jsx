21fa2377e4623a7dcf3561b5f7ec431be8288210
rstacks <stacksrob910@gmail.com>
2023-12-31 15:42:16 -0600

    Created new main nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import "./styles.css"
 function App() {
   return (
     <>
-      <p>Howdy! Welcome to my website. I</p>
+      {/* Nav bar */}
+      <nav id="main-nav">
+        <ul id="main-nav-list">
+          <li className="main-nav-link">HOME</li>
+          <li className="main-nav-link">ABOUT</li>
+          <li className="main-nav-link">PORTFOLIO</li>
+          <li className="main-nav-link">MORE</li>
+        </ul>
+      </nav>
     </>
   );
 }
