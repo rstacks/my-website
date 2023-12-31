d09ac55020e2309f19d76d887a7134cac9ce17c2
rstacks <stacksrob910@gmail.com>
2023-12-31 17:22:56 -0600

    Added header (name and info) to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
       <main>
         <img src="images/circular_me.png" alt="Robert Stacks Photo" 
           id="my-photo" />
-        
+        <h1 id="name-tag">Robert Stacks</h1>
+        <h2 id="tagline">Computer Science @ Texas A&M University</h2>
       </main>
     </>
   );
