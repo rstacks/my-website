381cd9b4c557ba302d931d5104ef3107b7a209a9
rstacks <stacksrob910@gmail.com>
2023-12-31 15:50:12 -0600

    Moved main nav bar into its own React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-// Should include import for stylesheet
 import "./styles.css"
+import { Nav } from "./Nav"
 //import { useState } from 'react'
 
 function App() {
   return (
     <>
-      {/* Nav bar */}
-      <nav id="main-nav">
-        <ul id="main-nav-list">
-          <li className="main-nav-link">HOME</li>
-          <li className="main-nav-link">ABOUT</li>
-          <li className="main-nav-link">PORTFOLIO</li>
-          <li className="main-nav-link">MORE</li>
-        </ul>
-      </nav>
+      <Nav />
     </>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+export function Nav() {
+  return (
+    <>
+      <nav id="main-nav">
+        <ul id="main-nav-list">
+          <li className="main-nav-link">HOME</li>
+          <li className="main-nav-link">ABOUT</li>
+          <li className="main-nav-link">PORTFOLIO</li>
+          <li className="main-nav-link">MORE</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
