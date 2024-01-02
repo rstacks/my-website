19a23d2316a216861c63902d987df8d0d5dc0a9d
rstacks <stacksrob910@gmail.com>
2024-01-02 15:16:08 -0600

    Added 404 Not Found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
 //import { useState } from 'react'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
