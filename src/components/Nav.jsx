a9568edd06775dfea581147898a7612405a70b30
rstacks <stacksrob910@gmail.com>
2024-01-02 15:57:35 -0600

    Made "Home" link in navbar clickable

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,9 @@ export function Nav() {
     <>
       <nav id="main-nav">
         <ul className="nav-list">
-          <li className="main-nav-link">HOME</li>
+          <li className="main-nav-link">
+            <a href="/home" className="main-nav-anchor">HOME</a>
+          </li>
           <li className="main-nav-link">ABOUT</li>
           <li className="main-nav-link">PORTFOLIO</li>
           <li className="main-nav-link">MORE</li>
