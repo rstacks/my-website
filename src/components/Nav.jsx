c9e596a5e47739de7cdba2972639c32b542a8898
rstacks <stacksrob910@gmail.com>
2024-01-02 18:20:30 -0600

    Began creating nav menu dropdown button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,10 @@ export function Nav() {
   return (
     <>
       <nav id="main-nav">
+        <button id="menu-button">
+          <img src="images/hamburger.png" alt="Menu" id="burger" />
+          MENU
+        </button>
         <ul className="nav-list">
           <li className="main-nav-link">
             <a href="/home" className="main-nav-anchor">HOME</a>
