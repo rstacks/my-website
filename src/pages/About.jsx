3b2066789d7c0ecb510dde7342345a3d0d8886f1
rstacks <stacksrob910@gmail.com>
2024-01-04 20:58:32 -0600

    Planned some of the text for ABOUT page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,25 @@ export function About() {
       </Helmet>
       <Nav />
       <main>
-        <p>cicj</p>
+        <p>
+          Howdy! As I'm sure you've gathered, I'm Robert, a sophomore computer science
+          major at Texas A&M University. Here are some quick facts about me:
+          - I'm a teaching assistant AND a peer teacher for the Texas A&M
+          Department of Computer Science and Engineering
+          - I worked on some cool projects with the Aggie Coding Club
+          - I've become interested in cybersecurity with the help of the
+          Texas A&M Cybersecurity Club
+          - I recently earned a GFACT certification from GIAC
+          - I'm currently studying for the CompTIA Security+ certification
+
+          Maybe link to ACC and CyberClub websites?
+
+          If you're interested in learning more, have a look around!
+
+          link
+          Here's some more quick facts:
+
+        </p>
       </main>
       <Footer />
     </HelmetProvider>
