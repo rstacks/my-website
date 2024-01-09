7ab7149f4a0f4c2cb9c130103f8c14ff050cae8a
rstacks <stacksrob910@gmail.com>
2024-01-09 17:58:54 -0600

    Added functionality to projects button

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,26 @@ import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 
 export function Portfolio() {
+  // Shows or hides content when a dropdown button is used
+  function dropdownButton(buttonName) {
+    // Get HTML tags to show or hide
+    const project1 = document.getElementById("project1");
+    const project2 = document.getElementById("project2");
+
+    if (buttonName === "projects") {
+      const button = document.getElementById("projects-button");
+      if (button.innerHTML != "<h1>- Projects</h1>") {
+        button.innerHTML = "<h1>- Projects</h1>";
+        project1.style.display = "block";
+        project2.style.display = "block";
+      } else {
+        button.innerHTML = "<h1>+ Projects</h1>";
+        project1.style.display = "none";
+        project2.style.display = "none";
+      }
+    }
+  }
+  
   return (
     <HelmetProvider>
       <Helmet>
@@ -14,8 +34,22 @@ export function Portfolio() {
       <Header text="PORTFOLIO" />
       <main>
         <header className="portfolio-dropdown">
-          <h1>+ Projects</h1>
+          <button id="projects-button" onClick={() => {
+            dropdownButton("projects");
+          }}>
+            <h1>+ Projects</h1>
+          </button>
         </header>
+        <article className="article-body" id="project1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada fames ac. Sodales neque sodales ut etiam sit amet nisl purus in. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Sem viverra aliquet eget sit. Viverra tellus in hac habitasse platea dictumst vestibulum. Augue interdum velit euismod in pellentesque.
+          </p>
+        </article>
+        <article className="article-body" id="project2">
+          <p>
+            Lobortis scelerisque fermentum dui faucibus in ornare. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Ut consequat semper viverra nam libero justo laoreet. Tempus quam pellentesque nec nam aliquam sem et tortor. Pulvinar mattis nunc sed blandit libero volutpat. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Tincidunt ornare massa eget egestas. Id leo in vitae turpis massa. Tristique et egestas quis ipsum suspendisse ultrices. Vel quam elementum pulvinar etiam. Leo a diam sollicitudin tempor id eu. Dui ut ornare lectus sit amet.
+          </p>
+        </article>
       </main>
 
       <Footer />
