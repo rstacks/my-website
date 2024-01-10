8ea196ecbcb088b443f4e74fad431b67813dbd9f
rstacks <stacksrob910@gmail.com>
2024-01-10 17:31:37 -0600

    Added Maroon Engine to project entries

--- src/components/ProjectEntry.jsx
+++ src/components/ProjectEntry.jsx
@@ -1,18 +1,4 @@
 export function ProjectEntry(props) {
-  /**
-   * Props list:
-   * title = Personal Website
-   * imageSrc = "images/new-site-image.jpg"
-   * imageAlt = Website Image
-   * text =           This very website was built by me from scratch! My experience 
-          in web development was essentially nonexistent when I began this project,
-          and I'm happy with how things turned out. I recently redesigned 
-          this website's appearance, which gave me some experience with React and
-          responsive web design.
-   * techs = HTML, CSS, JavaScript, React
-   * github = https://github.com/rstacks/my-website
-   */
-  
   return (
     <article className="project-entry">
       <h2>{props.title}</h2>
