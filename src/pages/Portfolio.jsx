f9efd008e86fba0cd63bfc1b8d4cf2f3ce6eeed3
rstacks <stacksrob910@gmail.com>
2024-01-09 20:18:08 -0600

    Simplified HTML tag labels on Portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,13 +14,13 @@ export function Portfolio() {
       <Nav />
       <Header text="PORTFOLIO" />
       <main>
-        <PortfolioDropdown text="Projects" items="article-body" />
-        <article className="article-body" id="project1">
+        <PortfolioDropdown text="Projects" items="project-entry" />
+        <article className="project-entry">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada fames ac. Sodales neque sodales ut etiam sit amet nisl purus in. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Sem viverra aliquet eget sit. Viverra tellus in hac habitasse platea dictumst vestibulum. Augue interdum velit euismod in pellentesque.
           </p>
         </article>
-        <article className="article-body" id="project2">
+        <article className="project-entry">
           <p>
             Lobortis scelerisque fermentum dui faucibus in ornare. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Ut consequat semper viverra nam libero justo laoreet. Tempus quam pellentesque nec nam aliquam sem et tortor. Pulvinar mattis nunc sed blandit libero volutpat. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Tincidunt ornare massa eget egestas. Id leo in vitae turpis massa. Tristique et egestas quis ipsum suspendisse ultrices. Vel quam elementum pulvinar etiam. Leo a diam sollicitudin tempor id eu. Dui ut ornare lectus sit amet.
           </p>
