45354e404377ea7c597c29b03a94b4529de56452
rstacks <stacksrob910@gmail.com>
2024-06-17 16:55:32 -0500

    Style changes for writeup items

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -8,7 +8,7 @@ export function PortfolioSection(props) {
       <ul>
         <li>
           <PortfolioItem imageSrc="/images/uscg-2024-logo.png" imageAlt="USCG 2024 Logo"
-            title="Season IV, US Cyber Open: Beginner's Game Room" />
+            title="USCG Season IV: Beginner's Game Room" />
         </li>
         <li>
           <PortfolioItem imageSrc="/images/l3ak-ctf-logo.png" imageAlt="L3akCTF 2024 Logo"
