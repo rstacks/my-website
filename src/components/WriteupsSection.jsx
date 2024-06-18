bd7ba83e33ec743d72270237937e4f64c26b9d96
rstacks <stacksrob910@gmail.com>
2024-06-18 17:15:21 -0500

    More styling for writeups list and buttons

--- src/components/WriteupsSection.jsx
+++ src/components/WriteupsSection.jsx
@@ -1,11 +1,11 @@
 import { PortfolioItem } from "./PortfolioItem"
 
 export function WriteupsSection() {
-  const unorderedList = document.getElementById("writeups-list-scroller");
   // Amount to scroll by when a button is pressed
   const scrollDist = 150;
 
   function scrollLeft() {
+    const unorderedList = document.getElementById("writeups-list-scroller");
     unorderedList.scrollBy({
       left: -scrollDist,
       behavior: "smooth"
@@ -13,6 +13,7 @@ export function WriteupsSection() {
   }
 
   function scrollRight() {
+    const unorderedList = document.getElementById("writeups-list-scroller");
     unorderedList.scrollBy({
       left: scrollDist,
       behavior: "smooth"
@@ -28,7 +29,7 @@ export function WriteupsSection() {
         engineering, web exploitation, cryptography, digital forensics, and more!
       </p>
       <div id="writeups-list">
-        <button id="writeups-left-button" onClick={scrollLeft}>←</button>
+        <button id="writeups-left-button" onClick={scrollLeft}>⬅</button>
         <ul id="writeups-list-scroller">
           <li>
             <PortfolioItem imageSrc="/images/uscg-2024-logo.png" imageAlt="USCG 2024 Logo"
@@ -39,7 +40,7 @@ export function WriteupsSection() {
               title="L3akCTF 2024" />
           </li>
         </ul>
-        <button id="writeups-right-button" onClick={scrollRight}>→</button>
+        <button id="writeups-right-button" onClick={scrollRight}>➡</button>
       </div>
     </section>
   );
