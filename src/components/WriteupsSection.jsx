0221e77b36d5655299aa22e71de9105da2240a6f
rstacks <stacksrob910@gmail.com>
2024-06-18 16:55:00 -0500

    Added full functionality to writeup buttons

--- src/components/WriteupsSection.jsx
+++ src/components/WriteupsSection.jsx
@@ -1,6 +1,24 @@
 import { PortfolioItem } from "./PortfolioItem"
 
 export function WriteupsSection() {
+  const unorderedList = document.getElementById("writeups-list-scroller");
+  // Amount to scroll by when a button is pressed
+  const scrollDist = 150;
+
+  function scrollLeft() {
+    unorderedList.scrollBy({
+      left: -scrollDist,
+      behavior: "smooth"
+    });
+  }
+
+  function scrollRight() {
+    unorderedList.scrollBy({
+      left: scrollDist,
+      behavior: "smooth"
+    });
+  }
+
   return (
     <section className="portfolio-section-header">
       <h2>Writeups</h2>
@@ -10,8 +28,8 @@ export function WriteupsSection() {
         engineering, web exploitation, cryptography, digital forensics, and more!
       </p>
       <div id="writeups-list">
-        <button id="writeups-left-button">←</button>
-        <ul>
+        <button id="writeups-left-button" onClick={scrollLeft}>←</button>
+        <ul id="writeups-list-scroller">
           <li>
             <PortfolioItem imageSrc="/images/uscg-2024-logo.png" imageAlt="USCG 2024 Logo"
               title="USCG Season IV: Beginner's Game Room" />
@@ -21,7 +39,7 @@ export function WriteupsSection() {
               title="L3akCTF 2024" />
           </li>
         </ul>
-        <button id="writeups-right-button">→</button>
+        <button id="writeups-right-button" onClick={scrollRight}>→</button>
       </div>
     </section>
   );
