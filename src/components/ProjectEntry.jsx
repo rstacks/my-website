c3458707a558e3db6296eb7d282ea0e745460e13
rstacks <stacksrob910@gmail.com>
2024-07-09 16:47:36 -0500

    Cleaned up stylesheet and come code from abandoned commits

--- src/components/ProjectEntry.jsx
+++ src/components/ProjectEntry.jsx
@@ -1,40 +1,6 @@
 export function ProjectEntry(props) {
-  /**
-   * None of this is done
-   * Goal was to implement clicking outside of the project entry leading to it closing
-   * But we don't want to be listening for outside clicks while the popup is invisible,
-   *  because that just made it never show up
-   *  
-  
-  
-  const hideThroughClick = (e) => {
-    const projectEntry = document.getElementById(props.id);
-    if (!(projectEntry.contains(e.target))) {
-      projectEntry.style.display = "none";
-    }
-  };
-  
-  function hideEntry() {
-    const projectEntry = document.getElementById(props.id);
-    projectEntry.style.display = "none";
-  }
-  
-  function clickOutsideHandler() {
-    const projectEntry = document.getElementById(props.id);
-    if (projectEntry.style.display === "block") {
-      projectEntry.addEventListener("click", hideThroughClick);
-    }
-  }
-
-  if (document.readyState === "loading" || document.readyState === "interactive") {
-    document.addEventListener("DOMContentLoaded", )
-  } else {
-
-  }
-  */
   return (
     <article className="project-entry" id={props.id}>
-      <button className="project-entry-button">×</button>
       <h2>{props.title}</h2>
       <section className="project-desc">
         <img src={props.imageSrc} alt={props.imageAlt} />
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,42 +2,26 @@ import { PortfolioItem } from "./PortfolioItem"
 /*import { ProjectEntry } from "./ProjectEntry"*/
 
 export function ProjectsSection() {
-  /*function showPopup(id) {
-    const popup = document.getElementById(id);
-    popup.style.display = "block";
-  }
-  */
   return (
-    <>
-      <section className="portfolio-section-header">
-        <h2>Projects</h2>
-        <p>
-          My computer science projects, including personal projects I worked on in
-          my spare time and school projects. Click on a tile below to learn more about
-          a specific project!
-        </p>
-        <div className="tile-list">
-          <ul>
-            <li>
-              <PortfolioItem imageSrc="/images/new-site-image.jpg" imageAlt="Website Image"
-                title="Personal Website" />
-            </li>
-            <li>
-              <PortfolioItem imageSrc="/images/maroon-engine-example-cropped.gif" imageAlt="Maroon Engine Image"
-                title="Maroon Engine" />
-            </li>
-          </ul>
-        </div>
-      </section>
-      {/*
-      <ProjectEntry id="maroon-engine-popup" title="Maroon Engine" imageSrc="/images/maroon-engine-example.gif"
-          imageAlt="Maroon Engine Image" text="A 2D side-scrolling game engine developed
-          as a results-focused project for the Aggie Coding Club. I joined a team of 13
-          students, and we each contributed to this project in our free time over the course
-          of three months. I assisted with the implementation of player collision
-          and enemy tracking behavior." techs="C, C++, OpenGL"
-          github="https://github.com/aggie-coding-club/maroon-engine" />
-      */}
-    </>
+    <section className="portfolio-section-header">
+      <h2>Projects</h2>
+      <p>
+        My computer science projects, including personal projects I worked on in
+        my spare time and school projects. Click on a tile below to learn more about
+        a specific project!
+      </p>
+      <div className="tile-list">
+        <ul>
+          <li>
+            <PortfolioItem imageSrc="/images/new-site-image.jpg" imageAlt="Website Image"
+              title="Personal Website" />
+          </li>
+          <li>
+            <PortfolioItem imageSrc="/images/maroon-engine-example-cropped.gif" imageAlt="Maroon Engine Image"
+              title="Maroon Engine" />
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
