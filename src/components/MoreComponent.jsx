99de0adf144cc8e2c1e888d352653efe16c30d00
rstacks <stacksrob910@gmail.com>
2024-07-22 12:14:57 -0500

    Added links to all badge tiles

--- src/components/MoreComponent.jsx
+++ src/components/MoreComponent.jsx
@@ -37,28 +37,46 @@ export function MoreComponent() {
           <button id="badges-left-button" className="scroll-button" onClick={scrollLeft}>⬅</button>
           <ul id="badge-list-scroller">
             <li>
-              <BadgeItem imageSrc="/images/uscg-badge.png"
-                imageAlt="USCG Beginner's Game Room 2024 Badge" />
+              <a href="https://api.badgr.io/public/assertions/wpQQcsY2SsGOFopkAGbV8Q"
+                target="_blank">
+                <BadgeItem imageSrc="/images/uscg-badge.png"
+                  imageAlt="USCG Beginner's Game Room 2024 Badge" />
+              </a>
             </li>
             <li>
-              <BadgeItem imageSrc="/images/gfact-badge.png"
-                imageAlt="GFACT Badge" />
+              <a href="https://www.credly.com/badges/943a6f7d-97c6-4f5d-8531-22f6880382cd/public_url"
+                target="_blank">
+                <BadgeItem imageSrc="/images/gfact-badge.png"
+                  imageAlt="GFACT Badge" />
+              </a>
             </li>
             <li>
-              <BadgeItem imageSrc="/images/cyber-honors-badge.png"
-                imageAlt="National Cyber Scholar with Honors Badge" />
+              <a href="https://api.badgr.io/public/assertions/w23l6LqzQhySRRV3U6h7LQ"
+                target="_blank">
+                <BadgeItem imageSrc="/images/cyber-honors-badge.png"
+                  imageAlt="National Cyber Scholar with Honors Badge" />
+              </a>
             </li>
             <li>
-              <BadgeItem imageSrc="/images/sans-badge.svg"
-                imageAlt="SANS Foundations Badge" />
+              <a href="https://api.badgr.io/public/assertions/7Xd_9G-_RI-EsDQ_MaMQnA"
+                target="_blank">
+                <BadgeItem imageSrc="/images/sans-badge.svg"
+                  imageAlt="SANS Foundations Badge" />
+              </a>
             </li>
             <li>
-              <BadgeItem imageSrc="/images/cyber-semifinalist-badge.png"
-                imageAlt="National Cyber Scholarship Semifinalist Badge" />
+              <a href="https://api.badgr.io/public/assertions/XV_YF-WrQH-Nu_3NWs8zMQ"
+                target="_blank">
+                <BadgeItem imageSrc="/images/cyber-semifinalist-badge.png"
+                  imageAlt="National Cyber Scholarship Semifinalist Badge" />
+              </a>
             </li>
             <li>
-              <BadgeItem imageSrc="/images/cyber-scholar-badge.png"
-                imageAlt="National Cyber Scholar Badge" />
+              <a href="https://api.badgr.io/public/assertions/XPUE7DJhRUiYce-wBdDyaw"
+                target="_blank">
+                <BadgeItem imageSrc="/images/cyber-scholar-badge.png"
+                  imageAlt="National Cyber Scholar Badge" />
+              </a>
             </li>
           </ul>
           <button id="badges-right-button" className="scroll-button" onClick={scrollRight}>➡</button>
