5f9d29e0b6da4382f2a2e03d4e0502c33e471b12
rstacks <stacksrob910@gmail.com>
2024-07-22 12:17:29 -0500

    Removed old MORE page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./styles.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Projects } from "./pages/Projects"
-import { More } from "./pages/More"
 import { NotFound } from "./pages/NotFound"
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/more" element={<More />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
